e66ad4415198a9b1c5a267a2d1c0b58ad7d77a8f
Chriee <christeena.sebastian@cloudium.io>
2021-09-16 16:17:21 +0530

    mongo

--- myApp.js
+++ myApp.js
@@ -50,6 +50,7 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
