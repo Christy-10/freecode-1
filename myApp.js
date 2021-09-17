7f3ccaf5002c32fddabbbda13675e40164c28108
Chriee <christeena.sebastian@cloudium.io>
2021-09-17 09:19:23 +0530

    model

--- myApp.js
+++ myApp.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+const mongoose=require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+const Person = mongoose.model("Person", personSchema);
+
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 
 let Person;
@@ -50,7 +60,6 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
